0c086a712b868fe451f5c8c24c2abdad5bb6a8fc
Raghav Kuppan <raghav.kuppan@outlook.com>
2024-12-15 19:14:02 +0530

    Day 15 part 2

--- src/aoc15/mod.rs
+++ src/aoc15/mod.rs
@@ -10,7 +10,7 @@ type Num = i32;
 
 type Pos = (Num, Num);
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Move {
     Up,
     Down,
@@ -23,6 +23,8 @@ enum Type {
     Bot,
     Box,
     Block,
+    BoxLeft,
+    BoxRight,
     Empty
 }
 
@@ -33,6 +35,8 @@ impl Debug for Type {
             Type::Box => write!(f, "O"),
             Type::Block => write!(f, "#"),
             Type::Empty => write!(f, "."),
+            Type::BoxLeft => write!(f, "["),
+            Type::BoxRight => write!(f, "]")
         }
     }
 }
@@ -41,14 +45,18 @@ impl Debug for Type {
 struct Context {
     bot: Pos,
     moves: Vec<Move>,
-    matrix: Vec<Vec<Type>>
+    matrix: Vec<Vec<Type>>,
+    bot2: Pos,
+    matrix2: Vec<Vec<Type>>,
 }
 
 impl Context {
     pub fn new(inp: Vec<String>) -> Context {
         let mut k = 0;
         let mut bot= (0, 0);
+        let mut bot2= (0, 0);
         let mut matrix: Vec<Vec<Type>> = vec![];
+        let mut matrix2: Vec<Vec<Type>> = vec![];
         let mut moves = vec![];
         loop {
             if inp[k].trim().is_empty() {
@@ -56,6 +64,7 @@ impl Context {
                 break;
             }
             let mut ve = vec![Type::Empty; inp[0].len()];
+            let mut ve2 = vec![];
             for (j, c) in inp[k].chars().enumerate() {
                 ve[j] = match c {
                     '#' => Type::Block,
@@ -65,9 +74,19 @@ impl Context {
                     },
                     'O' => Type::Box,
                     _ => Type::Empty,
+                };
+                match c {
+                    '#' => ve2.extend(vec![Type::Block, Type::Block]),
+                    '@' => {
+                        bot2 = (k as Num, ve2.len() as Num);
+                        ve2.extend(vec![Type::Bot, Type::Empty]);
+                    },
+                    'O' => ve2.extend(vec![Type::BoxLeft, Type::BoxRight]),
+                    _ => ve2.extend(vec![Type::Empty, Type::Empty]),
                 }
             }
             matrix.push(ve);
+            matrix2.push(ve2);
             k += 1;
         }
         while k < inp.len() {
@@ -83,13 +102,21 @@ impl Context {
         Context {
             bot: bot,
             moves: moves,
-            matrix: matrix
+            matrix: matrix,
+            bot2: bot2,
+            matrix2: matrix2
         }
     }
 
     pub fn print_matrix(&self) {
         for i in 0..self.matrix.len() {
             println!("{:?}", self.matrix[i]);
+        }
+    }
+
+    pub fn print_matrix2(&self) {
+        for i in 0..self.matrix2.len() {
+            println!("{:?}", self.matrix2[i]);
         }
     }
 
@@ -105,23 +132,35 @@ impl Context {
         sum
     }
 
-    fn move_bot(&mut self, i: usize) {
-        //println!("Bot position: {:?}; Move: {:?}", self.bot, self.moves[i]);
-        let add = match self.moves[i] {
+    pub fn compute_gps2(&self) -> usize {
+        let mut sum = 0;
+        for i in 0..self.matrix2.len() {
+            for j in 0..self.matrix2[0].len() {
+                if self.matrix2[i][j] == Type::BoxLeft {
+                    sum += 100 * i + j;
+                }
+            }
+        }
+        sum
+    }
+
+    fn move_bot(bot: &mut Pos, matrix: &mut Vec<Vec<Type>>, moves: &Vec<Move>, i: usize) {
+        //println!("Bot position: {:?}; Move: {:?}", bot, moves[i]);
+        let add = match moves[i] {
             Move::Right => (0, 1),
             Move::Left => (0, -1),
             Move::Down => (1, 0),
             Move::Up => (-1, 0)
         };
-        let nex_bot = (self.bot.0 + add.0, self.bot.1 + add.1);
+        let nex_bot = (bot.0 + add.0, bot.1 + add.1);
         let nex_botu = (nex_bot.0 as usize, nex_bot.1 as usize);
-        if self.matrix[nex_botu.0][nex_botu.1] == Type::Empty {
-            self.matrix[nex_botu.0][nex_botu.1] = Type::Bot;
-            self.matrix[self.bot.0 as usize][self.bot.1 as usize] = Type::Empty;
-            self.bot = nex_bot;
+        if matrix[nex_botu.0][nex_botu.1] == Type::Empty {
+            matrix[nex_botu.0][nex_botu.1] = Type::Bot;
+            matrix[bot.0 as usize][bot.1 as usize] = Type::Empty;
+            *bot = nex_bot;
             return;
         }
-        if self.matrix[nex_botu.0][nex_botu.1] == Type::Block {
+        if matrix[nex_botu.0][nex_botu.1] == Type::Block {
             return;
         }
         // If block ->
@@ -132,10 +171,10 @@ impl Context {
         let (mut i, mut j) = nex_bot;
         loop {
             let (iu, ju) = (i as usize, j as usize);
-            if self.matrix[iu][ju] == Type::Block {
+            if matrix[iu][ju] == Type::Block {
                 return;
             }
-            if self.matrix[iu][ju] == Type::Box {
+            if matrix[iu][ju] == Type::Box || matrix[iu][ju] == Type::BoxLeft || matrix[iu][ju] == Type::BoxRight {
                 i += add.0;
                 j += add.1;
                 continue;
@@ -145,25 +184,113 @@ impl Context {
         //println!("Next Empty position at: ({}, {})", i, j);
         loop {
             let (ni, nj): (i32, i32) = (i - add.0, j - add.1);
-            self.matrix[i as usize][j as usize] = self.matrix[ni as usize][nj as usize];
+            matrix[i as usize][j as usize] = matrix[ni as usize][nj as usize];
             if i == nex_bot.0 && j == nex_bot.1 {
                 break;
             }
             i -= add.0;
             j -= add.1;
         }
-        self.matrix[nex_botu.0][nex_botu.1] = Type::Bot;
-        self.matrix[self.bot.0 as usize][self.bot.1 as usize] = Type::Empty;
-        self.bot = nex_bot;
+        matrix[nex_botu.0][nex_botu.1] = Type::Bot;
+        matrix[bot.0 as usize][bot.1 as usize] = Type::Empty;
+        *bot = nex_bot;
     }
 
     pub fn part1(&mut self) -> usize {
         //self.print_matrix();
         for i in 0..self.moves.len() {
-            self.move_bot(i);
+            //println!("");
+            Context::move_bot(&mut self.bot, &mut self.matrix, &self.moves, i);
         }
         //self.print_matrix();
         self.compute_gps()
+    }
+
+    fn check_column(cur_pos: Pos, m: &Move, matrix: &Vec<Vec<Type>>) -> bool {
+        let add = match m {
+            Move::Up => (-1, 0),
+            Move::Down => (1, 0),
+            _ => panic!("Cannot do this for lateral moves")
+        };
+        let nex_pos = (cur_pos.0 + add.0, cur_pos.1 + add.1);
+        let nex_posu = (nex_pos.0 as usize, nex_pos.1 as usize);
+        let cur_posu = (cur_pos.0 as usize, cur_pos.1 as usize);
+        if matrix[nex_posu.0][nex_posu.1] == Type::Block {
+            return false;
+        }
+        if matrix[nex_posu.0][nex_posu.1] == Type::Empty {
+            return true;
+        }
+        if matrix[nex_posu.0][nex_posu.1] == Type::BoxLeft {
+            return Context::check_column((nex_pos.0, nex_pos.1), m, matrix) &&
+                Context::check_column((nex_pos.0, nex_pos.1 + 1), m, matrix);
+        }
+        if matrix[nex_posu.0][nex_posu.1] == Type::BoxRight {
+            return Context::check_column((nex_pos.0, nex_pos.1), m, matrix) &&
+            Context::check_column((nex_pos.0, nex_pos.1 - 1), m, matrix);
+        }
+        panic!("Unknown type for check_column: {:?}", matrix[nex_posu.0][nex_posu.1]);
+    }
+
+    fn move_column(cur_pos: Pos, m: &Move, matrix: &mut Vec<Vec<Type>>) -> Pos {
+        let add = match m {
+            Move::Up => (-1, 0),
+            Move::Down => (1, 0),
+            _ => panic!("Cannot do this for lateral moves")
+        };
+        let nex_pos = (cur_pos.0 + add.0, cur_pos.1 + add.1);
+        let nex_posu = (nex_pos.0 as usize, nex_pos.1 as usize);
+        let cur_posu = (cur_pos.0 as usize, cur_pos.1 as usize);
+        if matrix[nex_posu.0][nex_posu.1] == Type::Block {
+            panic!("Cannot move {:?} with move {:?}", cur_pos, m);
+        }
+        if matrix[nex_posu.0][nex_posu.1] == Type::Empty {
+            matrix[nex_posu.0][nex_posu.1] = matrix[cur_posu.0][cur_posu.1];
+            matrix[cur_posu.0][cur_posu.1] = Type::Empty;
+            //println!("Cur_pos: {:?}, Value: {:?}, new_pos: {:?}", cur_pos, matrix[cur_posu.0][cur_posu.1], nex_pos);
+            return nex_pos;
+        }
+        if matrix[nex_posu.0][nex_posu.1] == Type::BoxLeft {
+            Context::move_column((nex_pos.0, nex_pos.1), m, matrix);
+            Context::move_column((nex_pos.0, nex_pos.1 + 1), m, matrix);
+            matrix[nex_posu.0][nex_posu.1] = matrix[cur_posu.0][cur_posu.1];
+            matrix[cur_posu.0][cur_posu.1] = Type::Empty;
+            //println!("Cur_pos: {:?}, Value: {:?}, new_pos: {:?}", cur_pos, matrix[cur_posu.0][cur_posu.1], nex_pos);
+            return nex_pos;
+        }
+        if matrix[nex_posu.0][nex_posu.1] == Type::BoxRight {
+            Context::move_column((nex_pos.0, nex_pos.1), m, matrix);
+            Context::move_column((nex_pos.0, nex_pos.1 - 1), m, matrix);
+            matrix[nex_posu.0][nex_posu.1] = matrix[cur_posu.0][cur_posu.1];
+            matrix[cur_posu.0][cur_posu.1] = Type::Empty;
+            //println!("Cur_pos: {:?}, Value: {:?}, new_pos: {:?}", cur_pos, matrix[cur_posu.0][cur_posu.1], nex_pos);
+            return nex_pos;
+        }
+        panic!("Unknown type for check_column: {:?}", matrix[nex_posu.0][nex_posu.1]);
+    }
+
+    fn move_bot_2(bot: &mut Pos, matrix: &mut Vec<Vec<Type>>, moves: &Vec<Move>, i: usize) {
+        if moves[i] == Move::Left || moves[i] == Move::Right {
+            Context::move_bot(bot, matrix, moves, i);
+            return;
+        }
+        //println!("Vertical move");
+        if Context::check_column(*bot, &moves[i], &matrix) {
+            let nex_pos = Context::move_column(*bot, &moves[i], matrix);
+            matrix[bot.0 as usize][bot.1 as usize] = Type::Empty;
+            *bot = nex_pos;
+        }
+    }
+
+    pub fn part2(&mut self) -> usize {
+        //self.print_matrix2();
+         for i in 0..self.moves.len() {
+            //println!("");
+            Context::move_bot_2(&mut self.bot2, &mut self.matrix2, &self.moves, i);
+            //self.print_matrix2();
+         }
+        //self.print_matrix2();  
+        self.compute_gps2()
     }
 
 }
@@ -181,7 +308,7 @@ mod aoc15 {
         //println!("Example part1: {}", part1);
         let part1 = context.part1();
         println!("Example1 Part1: {:?}", part1);
-        //println!("Example1 Part2: {:?}", context.part2());
+        println!("Example1 Part2: {:?}", context.part2());
     }
 
     #[test]
@@ -190,11 +317,21 @@ mod aoc15 {
         let mut context = Context::new(text);
         //println!("context: {:?}", context);
         let part1 = context.part1();
-        println!("Example part1: {}", part1);
+        println!("Example2 part1: {}", part1);
         //let (part1, area) = context.part1();
         //println!("Example1 Part1: {:?}", part1);
-        //println!("Example1 Part2: {:?}", context.part2());
-        //context.part2(1000, 11, 7);
+        println!("Example2 Part2: {:?}", context.part2());
+    }
+
+    #[test]
+    fn example3() {
+        let text: Vec<String> = read_input("src/aoc15/example3").expect("couldn't read input - aoc15");;
+        let mut context = Context::new(text);
+        let part1 = context.part1();
+        println!("Example3 part1: {}", part1);
+        //let part1 = context.part1();
+        //println!("Example3 Part1: {:?}", part1);
+        println!("Example3 Part2: {:?}", context.part2());
     }
 
    #[test]
@@ -203,7 +340,6 @@ mod aoc15 {
         let mut context = Context::new(text);
         let part1 = bench(|| context.part1(), Some("part1"));
         println!("Part1: {:?}", part1);
-        //println!("Part2: {:?}", bench(|| context.part2(), Some("part2")));
-        //bench(|| context.part2(100000, 101, 103), Some("Part2"));
+        println!("Part2: {:?}", bench(|| context.part2(), Some("part2")));
     }
 }
